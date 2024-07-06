1f55f3575df1e69a6076d22dc3c4097f2ec351dc
Charles Zhang <shhwkk54@gmail.com>
2024-07-05 17:57:56 -0700

    fix: change img with Image

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,95 +1,121 @@
+import Image from "next/image";
+
 export default function Gallery() {
   return (
-    <div class="container py-10 grid grid-cols-2 md:grid-cols-4 gap-4">
-      <div class="flex flex-col gap-4">
+    <div className="container py-10 grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="flex flex-col gap-4">
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg transition-all duration-300 group-hover:scale-110 group-hover:rotate-6"
+          <Image
+            className="h-auto max-w-full rounded-lg transition-all duration-300 group-hover:scale-110 group-hover:rotate-6"
             src="/image/badroom.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery2.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
       </div>
-      <div class="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/swiper1.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery11.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/living.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
       </div>
-      <div class="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery14.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery15.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery12.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
       </div>
-      <div class="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery7.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery11.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
         <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
+          <Image
+            className="h-auto max-w-full rounded-lg"
             src="/image/gallery3.jpg"
             alt=""
+            width={500} // Provide appropriate width
+            height={500} // Provide appropriate height
           />
         </div>
       </div>
